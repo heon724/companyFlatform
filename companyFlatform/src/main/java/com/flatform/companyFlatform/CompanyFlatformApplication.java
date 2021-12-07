bde7dfae47c23a8935e0935e2ca2052cd73e6d06
heon724 <kdongh94@naver.com>
2021-12-07 17:37:05 +0900

    init

---
+++ companyFlatform/src/main/java/com/flatform/companyFlatform/CompanyFlatformApplication.java
@@ -0,0 +1,13 @@
+package com.flatform.companyFlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyFlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyFlatformApplication.class, args);
+	}
+
+}
---
+++ companyFlatform/src/test/java/com/flatform/companyFlatform/CompanyFlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flatform.companyFlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyFlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
